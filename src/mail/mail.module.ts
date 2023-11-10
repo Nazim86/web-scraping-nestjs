07059b61223b8be52d2c048e3d3ce6ce1506d8ba
Nazim <nazim86@gmail.com>
2023-11-11 02:32:12 +0400

    add dotenv.config()

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -5,10 +5,10 @@ import { MailService } from './mail.service';
 import { join } from 'path';
 
 import * as process from 'process';
+import * as dotenv from 'dotenv';
 
 // Load environment variables from .env
-//dotenv.config();
-
+dotenv.config();
 @Module({
   imports: [
     MailerModule.forRoot({
