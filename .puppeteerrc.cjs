5c1991520b0e45d44a291b1ca5581405adef17f1
Nazim <nazim86@gmail.com>
2023-11-10 13:10:04 +0400

    change yarn to npm

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,5 +1,5 @@
-const {join} = require('path');
-//import { join } from 'path';
-module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
-};
+// const {join} = require('path');
+// //import { join } from 'path';
+// module.exports = {
+//   cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+// };
